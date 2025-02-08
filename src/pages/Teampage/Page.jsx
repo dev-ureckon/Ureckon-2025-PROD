d94fdc0c1700f0304b0225f05424fb57d1b1eb7f
ArnabhS <sinhaarnabh888@gmail.com>
2025-02-08 13:25:18 +0530

    fix: team page responsiveness

--- src/pages/Teampage/Page.jsx
+++ src/pages/Teampage/Page.jsx
@@ -5,7 +5,7 @@ const domains = ["TECH", "COORD", "GRAPHICS", "BRANDING", "CORE", "SOCIAL MEDIA"
 const TeamPage = () => {
   return (
     <div
-      className="bg-black text-white w-full h-screen flex flex-col items-center justify-center relative font-press"
+      className=" text-white w-full h-screen flex flex-col items-center justify-center relative font-press mx-auto"
     >
       <Header />
       <CenterSection />
@@ -14,21 +14,21 @@ const TeamPage = () => {
 };
 
 const Header = () => (
-        <div className="text-red-500 text-xl font-bold tracking-widest text-shadow-red text-center sm:text-lg">
+        <div className="text-red-500 text-lg md:text-xl font-bold tracking-widest text-shadow-red text-center ">
           THE OFFICIAL TEAM OF URECKON'25
         </div>
 );
 
 const CenterSection = () => (
-  <div className="text-center mt-12 z-10">
-    <h2 className="text-xl tracking-wider text-gray-200 mb-20 text-glow-white">
+  <div className="text-center ">
+    <h2 className="text-base md:text-xl  tracking-wider text-gray-200 mb-10 text-glow-white mt-10">
       SELECT THE DOMAIN
     </h2>
-    <div className="grid grid-cols-1 gap-6 sm:grid-cols-3">
+    <div className="grid grid-cols-1 gap-6 sm:grid-cols-3 mx-4">
       {domains.map((domain, idx) => (
         <button
           key={idx}
-          className="bg-red-950 hover:bg-red-950/90 text-white py-8 px-4 font-bold tracking-wide"
+          className="bg-red-950 hover:bg-red-950/90 text-white md:py-8 py-4 px-4 font-bold tracking-wide"
         >
           {domain}
         </button>
