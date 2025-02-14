7074852edceeb2c42671ae050fc39a21aeb8b395
Sayan Snigdha Pal <sayanpalb@gmail.com>
2025-02-15 04:08:49 +0530

    changes

--- src/pages/Teampage/Page.jsx
+++ src/pages/Teampage/Page.jsx
@@ -1,37 +1,43 @@
-import React from "react";
-
-const domains = ["TECH", "COORD", "GRAPHICS", "BRANDING", "CORE", "SOCIAL MEDIA"];
+import React, { useEffect, useState } from "react";
+import teamsData from "../../lib/team_data_routes.json";
+import { Link } from "react-router";
+import SectionContainer from "../../components/SectionContainer";
 
 const TeamPage = () => {
+  const [domains, setDomains] = useState([]);
+
+  useEffect(() => {
+    setDomains(teamsData);
+  }, []);
+
   return (
-    <div
-      className=" text-white w-full h-screen flex flex-col items-center justify-center relative font-press mx-auto"
-    >
-      <Header />
-      <CenterSection />
-    </div>
+    <SectionContainer>
+      <div className="text-white w-full h-screen flex flex-col items-center justify-center relative font-press mx-auto">
+        <Header />
+        <CenterSection domains={domains} />
+      </div>
+    </SectionContainer>
   );
 };
 
 const Header = () => (
-        <div className="text-red-500 text-lg md:text-xl font-bold tracking-widest text-shadow-red text-center ">
-          THE OFFICIAL TEAM OF URECKON'25
-        </div>
+  <div className="text-red-500 mt-32 text-lg md:text-xl font-bold tracking-widest text-shadow-red text-center">
+    THE OFFICIAL TEAM OF URECKON'25
+  </div>
 );
 
-const CenterSection = () => (
-  <div className="text-center ">
-    <h2 className="text-base md:text-xl  tracking-wider text-gray-200 mb-10 text-glow-white mt-10">
+const CenterSection = ({ domains }) => (
+  <div className="text-center">
+    <h2 className="text-base md:text-xl tracking-wider text-gray-200 mb-10 text-glow-white mt-10">
       SELECT THE DOMAIN
     </h2>
     <div className="grid grid-cols-1 gap-6 sm:grid-cols-3 mx-4">
       {domains.map((domain, idx) => (
-        <button
-          key={idx}
-          className="bg-red-950 hover:bg-red-950/90 text-white md:py-8 py-4 px-4 font-bold tracking-wide"
-        >
-          {domain}
-        </button>
+        <Link key={idx} to={`/teams/${domain.route}`} className="w-full">
+          <button className="bg-red-950 hover:bg-red-950/90 text-white md:py-8 py-4 px-6 font-bold tracking-wide w-full min-w-[150px]">
+            {domain.name}
+          </button>
+        </Link>
       ))}
     </div>
   </div>
